ab56caa2a3ac9c4efd4656a508f09b00e0f8195e
Rafał Mazur <raf.mazur.000@gmail.com>
2026-05-23 20:39:20 +0200

    Implemented AvgDistanceManager

--- src/xd_random_walk/plot_managers/avg_plot_managers/AvgDistanceManager.py
+++ src/xd_random_walk/plot_managers/avg_plot_managers/AvgDistanceManager.py
@@ -1,16 +1,63 @@
 from matplotlib.axes import Axes
 from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
 import numpy as np
 
 class AvgDistanceManager:
-    _avarages: np.ndarray
-    _steps: np.ndarray
+    _averages: list[float]
+    _step: int
 
     _fig: Figure
     _ax: Axes
 
+    _a: float
 
     def __init__(self) -> None:
-        pass
+        self._fig = plt.figure('Avarage distance')
+        self._ax = self._fig.add_subplot(111)
 
-    def 
+        self._ax.set_xlabel("N (steps)")
+        self._ax.set_ylabel("Distance from Origin")
+        self._ax.grid(True, linestyle='--', alpha=0.5)
+
+        self._averages = []
+        self._step = 0
+
+        self._avg_line = self._ax.plot([], [], label="Current Step Avg", color="blue")[0]
+        self._regression_line = self._ax.plot([], [], label="a*sqrt(N)", color="red", linestyle="--")[0]
+        
+        self._ax.legend()
+    
+    def _fit_a(self):
+        if self._step < 2:
+            self._a = 1.0
+            return
+        
+        np_avgs_points = np.array(self._averages)
+        np_regr_points = np.arange(1, self._step + 1)
+
+        self._a = float(np.sum(np_avgs_points * (np_regr_points ** 0.5)) / np.sum(np_regr_points))
+    
+    def update_data(self, new_current_avg: float, new_global_avg: float):
+        self._averages.append(new_current_avg)
+        self._step += 1
+        self._fit_a()
+    
+    def _update_plot(self):
+        steps = np.arange(1, self._step + 1)
+        self._avg_line.set_data(steps, self._averages)
+        
+        theoretical_y = [self._a * (n**0.5) for n in steps]
+        self._regression_line.set_data(steps, theoretical_y)
+        
+        if self._step > 0:
+            self._ax.set_xlim(0, max(10, self._step * 1.05))
+            
+            max_y = max(max(self._averages), max(theoretical_y), 1)
+            self._ax.set_ylim(0, max_y * 1.1)
+
+        # Return both lines for FuncAnimation
+        return self._avg_line, self._regression_line
+
+    def update(self):
+        return self._update_plot()
