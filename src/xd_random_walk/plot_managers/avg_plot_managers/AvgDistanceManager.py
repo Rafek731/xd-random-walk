f4f1fcea7eb28f4b61df6475c1d0f6c1074ba440
Rafał Mazur <raf.mazur.000@gmail.com>
2026-05-23 17:46:17 +0200

    AvgDistanceManager  class

---
+++ src/xd_random_walk/plot_managers/avg_plot_managers/AvgDistanceManager.py
@@ -0,0 +1,16 @@
+from matplotlib.axes import Axes
+from matplotlib.figure import Figure
+import numpy as np
+
+class AvgDistanceManager:
+    _avarages: np.ndarray
+    _steps: np.ndarray
+
+    _fig: Figure
+    _ax: Axes
+
+
+    def __init__(self) -> None:
+        pass
+
+    def 
